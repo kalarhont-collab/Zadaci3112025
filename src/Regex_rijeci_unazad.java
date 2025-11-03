e8c01478549f8f64614d2b14aaaa71ede764ed2d
IvanSabljić <kalarhont@gmail.com>
2025-11-03 20:56:44 +0100

    Zadatak 2

---
+++ src/Regex_rijeci_unazad.java
@@ -0,0 +1,24 @@
+//Napišite program u Javi koji će učitati niz znakova (string) sa standardnog ulaza i
+// okrenuti redoslijed riječi u unesenom stringu. Prikazati transformirani string s obrnutim redoslijedom riječi.
+//Primjer ulaza: Dobar dan svima
+//Primjer izlaza: svima dan Dobar
+
+import java.util.Scanner;
+public class Regex_rijeci_unazad {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Unesite tekst");
+
+        String unos = sc.nextLine();
+
+     String[] characters = unos.split("[\\s]");
+
+           for (int i = characters.length - 1; i >= 0; i--) {
+               System.out.printf(characters[i] + " ");
+
+}
+}
+}
